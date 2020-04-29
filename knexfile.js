21123ae07b8ca5c74d13493bc79929784e11a87d
Renato  Avellar <onizuka@Mac-mini-de-Renato.local>
2020-04-29 13:53:40 -0300

    configurando knex

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'knext_test',
+      user: 'postgres',
+      password:'jones666'
+    }
+  },
+
+ 
+
+};
